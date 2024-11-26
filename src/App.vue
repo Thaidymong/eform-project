027096707cb405816458f361c8bd5b06549ff35b
Thai Dymong <dt8393430@gmail.com>
2024-11-26 14:08:32 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Navbar from './views/Navbar/Navbar.vue';
+
+// import { RouterView } from 'vue-router';
+// import { NLoadingBarProvider } from 'naive-ui'; // Ensure this is also imported if not globally registered
+</script>
+
+<template>
+  <!-- <n-loading-bar-provider>
+    <RouterView />
+  </n-loading-bar-provider> -->
+  <Navbar/>
+</template>
+
+
+<style scoped>
+
+</style>
